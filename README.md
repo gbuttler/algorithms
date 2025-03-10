9260777e5fb7c345ce26dde497e48aa2921c75cb
Grace Buttler <grace.buttler@gmail.com>
2025-03-10 15:34:51 +0000

    complete fib solution

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# algorithms
+# Algorithms
+
+I want to use this repository to practise some fundamental computer science concepts.
+
+### Fibonacci
+
+- Using iteration, write a function fibs which takes a number and returns an array containing that many numbers from the Fibonacci sequence. Using an example input of 8, this function should return the array [0, 1, 1, 2, 3, 5, 8, 13].
+- Now write another function fibsRec which solves the same problem recursively.
+- Test both versions of your functions by passing in various lengths as arguments.
+
+_Inspired by 'The Odin Project'_
---
+++ fibonacci.js
@@ -0,0 +1,62 @@
+function fibs(runTime) {
+  fibArray = [];
+  let i = 0;
+  let j = 1;
+  fibArray.push(i);
+  fibArray.push(j);
+  while (runTime - 2 > 0) {
+    newNum = fibArray[fibArray.length - 2] + fibArray[fibArray.length - 1];
+    fibArray.push(newNum);
+    runTime--;
+  }
+  return fibArray;
+}
+
+console.log(fibs(8));
+
+//PSEUDOCODE
+//create a funtion that takes a variable dictating how many times it will run (8 in this case).
+//create an empty array
+//create a starting number, which is zero
+//add zero to the array
+// create a second number, which is 1
+//add 1 to the array
+//add i and j, and make i equal the result.
+//keep doing that until the array length is the same as the variable entered (8 in this case)
+//return the array (or console.log)
+
+function fibsRec(runTime) {
+  //   fibArray = [];
+  //   let i = 0;
+  //   let j = 1;
+  //   fibArray.push(i);
+  //   fibArray.push(j);
+  //   console.log("This was printed recursively");
+  //   if (runTime < 2) {
+  //     console.log(fibArray);
+  //     return [1];
+  //   } else {
+  //     return fibsRec(runTime - 1) + fibsRec(runTime - 2);
+  //   }
+  if (runTime < 2) {
+    return [0, 1];
+  } else {
+    let arrayFib = fibsRec(runTime - 1);
+    arrayFib.push(
+      arrayFib[arrayFib.length - 1] + arrayFib[arrayFib.length - 2]
+    );
+    return arrayFib;
+  }
+}
+
+console.log(`Recursive: ${fibsRec(3)}`);
+
+//PSEUDOCODE
+//create a funtion that takes a variable dictating how many times it will run (8 in this case).
+//create an empty array
+//create a starting number, which is zero
+//add zero to the array
+// create a second number, which is 1
+//add 1 to the array
+//if runTime <2 return the array
+//else take one away from runtile and then add the two last array elements together
